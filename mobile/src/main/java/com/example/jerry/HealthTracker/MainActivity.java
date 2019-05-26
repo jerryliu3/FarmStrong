e499464dc9075ca9c455918645fd11c733517048
Jerry Liu <jytliu@edu.uwaterloo.ca>
2019-05-26 14:42:49 -0700

    Remove most useless files and integrations (wear, google api, etc)

--- mobile/src/main/java/com/example/jerry/HealthTracker/MainActivity.java
+++ mobile/src/main/java/com/example/jerry/HealthTracker/MainActivity.java
@@ -36,21 +36,15 @@ import java.util.concurrent.TimeUnit;
 public class MainActivity extends AppCompatActivity {
     private static final long CONNECTION_TIME_OUT_MS = 100;
 
-    private GoogleApiClient client;
     private MobileServiceClient azureClient;
-    private String nodeId;
     private static final String TAG = "MainActivity";
-    private ArrayList<String> readings;
     TextView main;
     Button exit;
-    Switch toggleIntent;
-    boolean intentOn = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         main = (TextView) findViewById(R.id.helloworld);
-        readings = new ArrayList<String>();
         exit = (Button) findViewById(R.id.exit);
         /*try {
             azureClient = new MobileServiceClient(
@@ -59,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }*/
-        initApi();
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
     }
@@ -68,8 +61,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ClassifierActivity.class);
         startActivity(intent);
     }
-
-
 
     // Check if external storage is available to read and write
     public boolean isExternalStorageWritable() {
@@ -97,87 +88,13 @@ public class MainActivity extends AppCompatActivity {
          }*/
             File textFile = new File(newFile, externalfilename);
             FileWriter writer = new FileWriter(textFile, true);
-            for(int x=0;x<readings.size();x++) {
-                writer.append(readings.get(x) + "\n");
-            }
             writer.flush();
             writer.close();
-            readings = new ArrayList<String>();
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
-    }
-
-    public void resetData(View view) {
-        Log.i("jerry.HealthTracker", "Saving data");
-        try {
-            String externalfilename = "HeartRateData.txt";
-            if (isExternalStorageWritable()) {
-            } else {
-                main.setText("not writable");
-            }
-            File file = Environment.getExternalStorageDirectory();
-            File newFile = new File(file, "Wearable App");
-            if (!newFile.exists()) {
-                newFile.mkdirs();
-            }
-         /*int variable = 0;
-         if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-         {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, variable);
-         }*/
-            File textFile = new File(newFile, externalfilename);
-            FileWriter writer = new FileWriter(textFile, false);
-            writer.flush();
-            writer.close();
-            readings = new ArrayList<String>();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * Initializes the GoogleApiClient and gets the Node ID of the connected device.
-     */
-    private void initApi() {
-        client = getGoogleApiClient(this);
-        retrieveDeviceNode();
-    }
-
-    /**
-     * Returns a GoogleApiClient that can access the Wear API.
-     * @param context
-     * @return A GoogleApiClient that can make calls to the Wear API
-     */
-    private GoogleApiClient getGoogleApiClient(Context context) {
-        return new GoogleApiClient.Builder(context)
-                .addApi(Wearable.API)
-                .build();
-    }
-
-    /**
-     * Connects to the GoogleApiClient and retrieves the connected device's Node ID. If there are
-     * multiple connected devices, the first Node ID is returned.
-     */
-    private void retrieveDeviceNode() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                client.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
-                NodeApi.GetConnectedNodesResult result =
-                        Wearable.NodeApi.getConnectedNodes(client).await();
-                List<Node> nodes = result.getNodes();
-                if (nodes.size() > 0) {
-                    nodeId = nodes.get(0).getId();
-                }
-                client.disconnect();
-            }
-        }).start();
     }
 
     public void exit(View view){
